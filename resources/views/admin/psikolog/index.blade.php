28142dfab360742a015a0b5d9652e9fad66bf4c4
yurarizhadz <ryudhagen96@gmail.com>
2016-10-08 17:46:49 +0700

    piket

--- resources/views/admin/psikolog/index.blade.php
+++ resources/views/admin/psikolog/index.blade.php
@@ -28,7 +28,7 @@
     					<td>{{$psikos->no_hp}}</td>
     					<td><a href="{{url('/admin/psikolog/'.$psikos->id.'/edit')}}" class="btn tooltipped" data-tooltip = "edit" data-position="left"><i class = "mdi-editor-mode-edit"></i></a>
                         <a href="#" class="btn tooltipped" data-tooltip = "delete" data-position="top"><i class = "mdi-action-delete"></i></a>
-                        <a href="#" class="btn tooltipped modal-trigger" data-tooltip = "tambah piket" data-position="right"></i></a>
+                        <a href="#add-modal2" class="btn tooltipped modal-trigger" data-tooltip = "tambah piket" data-position="right"></i></a>
                         
                         </td>
     				</tr>
@@ -37,6 +37,54 @@
     	</table>
     </li>
 </ul>
+<div id="add-modal2" class="modal">
+    <nav class="task-modal-nav green">
+        <div class="nav-wrapper">
+            <div class="left col s12 m5 l5">
+                <ul>
+                    <li><a href="#!" class="todo-menu"><i class="modal-action modal-close  mdi-hardware-keyboard-backspace"></i></a></li>
+                    <li>Tambah Piket Psikolog</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="modal-content">                    
+        <div class="row" style="margin-left: 10px;margin-right: 10px;">
+        {!! Form::open(['url' => '/admin/psikolog',  'class' => 'control_form']) !!}
+            <div class="row_form">
+                <input type="checkbox" id="senin" />
+                <label for="senin">Senin</label>
+            </div>
+            <div class="row_form">
+                <input type="checkbox" id="selasa" />
+                <label for="selasa">Selasa</label>
+            </div>
+            <div class="row_form">
+                <input type="checkbox" id="rabu" />
+                <label for="rabu">Rabu</label>
+            </div>
+            <div class="row_form">
+                <input type="checkbox" id="kamis" />
+                <label for="kamis">Kamis</label>
+            </div>
+            <div class="row_form">
+                <input type="checkbox" id="jumat" />
+                <label for="jumat">Jum'at</label>
+            </div>
+            <div class="row_form">
+                <input type="checkbox" id="sabtu" />
+                <label for="sabtu">Sabtu</label>
+            </div>
+            <div class="row_form">
+                <input type="checkbox" id="minggu" />
+                <label for="minggu">Minggu</label>
+            </div>
+    <div class="modal-footer">
+        <button href="#" class="waves-effect waves-red btn-flat modal-action" type="submit">Save</button>
+    </div>
+    {!! Form::close() !!}
+    </div>
+</div>
 <div id="add-modal" class="modal">
     <nav class="task-modal-nav red">
         <div class="nav-wrapper">
